ded554d688d88ac6028897a1f17c3436956a1bec
JoshLRR <LoRoadRacing@gmail.com>
2025-05-04 03:49:08 -0700

    Changed CMake file to allow for separate testing of Qt app and unit testing, updated yaml to actually build code

--- QtApp/test/testCircularBuffer.cc
+++ QtApp/test/testCircularBuffer.cc
@@ -1,6 +1,22 @@
 #include "gtest/gtest.h"
 #include "CircularBuffer.h"
 
-TEST(CirculuarBuffer, Pop) {
-    
+TEST(CircularBufferTest, PushAndPop) {
+    CircularBuffer<int> buf(3);
+
+    EXPECT_TRUE(buf.isEmpty());
+
+    buf.push(1);
+    buf.push(2);
+    buf.push(3);
+
+    EXPECT_TRUE(buf.isFull());
+
+    auto val = buf.pop();
+    ASSERT_TRUE(val.has_value());
+    EXPECT_EQ(*val, 3); 
+
+    auto peek = buf.peek();
+    ASSERT_TRUE(peek.has_value());
+    EXPECT_EQ(*peek, 2);
 }
