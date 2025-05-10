6b5c6c657b3f3756f390bd53e4533cfa1b17dcc3
JoshLRR <LoRoadRacing@gmail.com>
2025-05-10 02:27:11 -0700

    Completed _pidTable codes, added some tests, fixed ConvertHexToInt edge case

--- QtApp/src/VehicleParser.cc
+++ QtApp/src/VehicleParser.cc
@@ -1,28 +1,65 @@
 #include "VehicleParser.h"
 
 VehicleParser::VehicleParser() {
-    pidRegistry = {
-        "SPEED"     : "",           // Buffer 0
-        "RPM"       : "",           // Buffer 1
-        "FUEL"      : "",           // Buffer 2
-        "WATERTEMP" : "",           // Buffer 3
-        "OILTEMP"   : "",           // Buffer 4
-        "GEAR"      : "",           // Buffer 5
-        "BATTVOLTS" : "",           // Buffer 6
-        "STOREDDTC" : "03",           // Buffer 7
+    _pidTable = {
+        {"SPEED"     , "010D"},           // Buffer 0
+        {"RPM"       , "010C"},           // Buffer 1
+        {"FUEL"      , "012F"},           // Buffer 2
+        {"WATERTEMP" , "0105"},           // Buffer 3
+        {"THROTTLE"  , "0111"},           // Buffer 4
+        {"OILTEMP"   , "015C"},           // Buffer 5
+        {"GEAR"      , "01A4"},           // Buffer 6
+        {"BATTVOLTS" , "0142"},           // Buffer 7
+        {"STOREDDTC" , "03"},             // Buffer 8
     };
 };
 
-std::string VehicleParser::Request() {
+/// @brief Makes a request to the device 
+/// @param request 
+/// @return 
+/// NOTE: This should be run in its own thread since it will probably block while waiting for a response from the vehicle
+std::optional<std::string> VehicleParser::Request(const std::string& request) {
+    std::string code = _pidTable[request];
 
-    return "";
+    /* TODO
+        Insert logic once we know what device we're running with
+    */
+
+    return code;
 }
 
-int VehicleParser::convertHexToInt() {
+/// @brief Converts a hex string to an integer
+/// @param hexString 
+/// @return Returns an integer with the converted hex string. Returns std::nullopt if conversion doesn't work
+std::optional<int> VehicleParser::ConvertHexToInt(const std::string& hexString) {
+    try {
+        size_t processed = 0;
+        int value = std::stoi(hexString, &processed, 16);
 
-    return 0;
+        // Make sure the entire string was processed (no trailing junk)
+        if (processed != hexString.size()) {
+            return std::nullopt;
+        }
+
+        return value;
+    } catch (const std::exception& e) {
+        std::cerr << "Hex conversion error: " << e.what() << '\n';
+        return std::nullopt;
+    }
 }
 
-std::string VehicleParser::FormRequestString() {
-    
+
+/// @brief Forms the OBD string to interface with the vehicle ECU.
+/// @param serviceMode The service mode to be used (OBD2 Standard, SAEJ1979)
+/// @param code The code to be used
+/// @return The OBD string
+std::string VehicleParser::FormRequestString(int serviceMode, std::string code) {
+
+}
+
+/// @brief Forms the OBD string to interface with the vehicle ECU.
+/// @param code The code to be used
+/// @return The OBD string
+std::string VehicleParser::FormRequestString(std::string code) {
+
 }
--- QtApp/test/TestVehicleParser.cc
+++ QtApp/test/TestVehicleParser.cc
@@ -1,6 +1,40 @@
 #include "gtest/gtest.h"
 #include "VehicleParser.h"
 
-TEST(VehicleParser, init) {
-    VehicleParser vp = VehicleParser();
+TEST(VehicleParser, convertHexToInt) {
+    VehicleParser parser;
+
+    // Valid hexadecimal strings
+    EXPECT_EQ(parser.ConvertHexToInt("0A"), 10);
+    EXPECT_EQ(parser.ConvertHexToInt("1F"), 31);
+    EXPECT_EQ(parser.ConvertHexToInt("FF"), 255);
+    EXPECT_EQ(parser.ConvertHexToInt("7fffffff"), 2147483647); // max signed int
+    EXPECT_EQ(parser.ConvertHexToInt("0"), 0);
+
+    // Lowercase hex (stoi accepts both)
+    EXPECT_EQ(parser.ConvertHexToInt("ff"), 255);
+    EXPECT_EQ(parser.ConvertHexToInt("deadbeef"), std::nullopt); // overflows signed int, expect nullopt
+
+    // Invalid hexadecimal strings
+    EXPECT_EQ(parser.ConvertHexToInt("G1"), std::nullopt);      // Invalid character
+    EXPECT_EQ(parser.ConvertHexToInt("123Z"), std::nullopt);    // Invalid character
+    EXPECT_EQ(parser.ConvertHexToInt(""), std::nullopt);        // Empty string
+
+    // Overflow test (larger than int max)
+    EXPECT_EQ(parser.ConvertHexToInt("FFFFFFFFF"), std::nullopt);
+}
+
+// Need to mock response from OBD2?
+TEST(VehicleParser, Request) {
+    VehicleParser parser;
+
+    EXPECT_EQ(parser.Request("SPEED"), "010D");
+    EXPECT_EQ(parser.Request("RPM"), "010C");
+    EXPECT_EQ(parser.Request("FUEL"), "012F");
+    EXPECT_EQ(parser.Request("WATERTEMP"), "0105");
+    EXPECT_EQ(parser.Request("THROTTLE"), "0111");
+    EXPECT_EQ(parser.Request("OILTEMP"), "015C");
+    EXPECT_EQ(parser.Request("GEAR"), "01A4");
+    EXPECT_EQ(parser.Request("BATTVOLTS"), "0142");
+    EXPECT_EQ(parser.Request("STOREDDTC"), "03");
 }
