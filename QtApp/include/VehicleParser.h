fef318c0d2e16dad3772538e7af5ea96e0ff73f1
JoshLRR <LoRoadRacing@gmail.com>
2025-05-10 02:27:11 -0700

    Completed _pidTable codes, added some tests, fixed ConvertHexToInt edge case

--- QtApp/include/VehicleParser.h
+++ QtApp/include/VehicleParser.h
@@ -1,17 +1,23 @@
 #ifndef VEHICLEPARSER_H
 #define VEHICLEPARSER_H
 
-#include <unordered_map>
 #include "CircularBufferManager.h"
+#include <unordered_map>
+#include <string>
+
 
 class VehicleParser {
 private:
-    std::unordered_map<std::string, int> pidRegistry;
+    std::unordered_map<std::string, std::string> _pidTable;
+    
+    std::string FormRequestString(int serviceMode, std::string code);
+    std::string FormRequestString(std::string code);
+    void PublishToMiddleware(int data);
 
 public:
     VehicleParser();
-    std::string Request(); 
-    int convertHexToInt();
+    std::optional<std::string> Request(const std::string& request); 
+    std::optional<int> ConvertHexToInt(const std::string& hexString);
 };
 
 #endif
