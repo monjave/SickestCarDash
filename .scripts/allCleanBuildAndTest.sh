0d67fc0b3340bb69d67f7f4d60431c1d2c87dc82
JoshLRR <LoRoadRacing@gmail.com>
2025-05-17 14:40:03 -0700

    oops forgot to save some changes to previous commit

---
+++ .scripts/allCleanBuildAndTest.sh
@@ -0,0 +1,17 @@
+set -e  # Exit on any command failure
+
+echo "[0/3] Removing previous build files"
+rm -rf build
+
+echo "[1/3] Configuring project with BUILD_GUI=OFF..."
+cmake -B build -S .
+
+echo "[2/3] Building project..."
+cmake --build build
+
+echo "[3/3] Running tests..."
+cd build/QtApp && ctest --output-on-failure
+
+cd ../../QtApp/test/build/Debug
+rm -rf "empty.csv" "malformed.csv" "valid.csv"
+cd ....
