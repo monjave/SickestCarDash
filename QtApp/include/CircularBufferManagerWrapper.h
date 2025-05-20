b87ff74fe15beece5e51fc6b3e53f845a1253f90
Xavier <xaviersignup@gmail.com>
2025-05-20 10:54:42 -0700

    Set the Speed and RPM dial needles to their respective properties

--- QtApp/include/CircularBufferManagerWrapper.h
+++ QtApp/include/CircularBufferManagerWrapper.h
@@ -27,21 +27,13 @@ class CircularBufferManagerWrapper : public QObject {
     Q_PROPERTY(bool parking READ parking WRITE setParking NOTIFY parkingChanged)
 
 public:
-    explicit CircularBufferManagerWrapper(QObject *parent = nullptr) : QObject(parent), m_speed(0),
-        m_rpm(0), m_fuel(0), m_temp(100), m_coolanttemp(0), m_clock(10000), m_enginetemp(0), m_oiltemp(0), m_gearshift(0), m_seatbelt(true),
-        m_highlights(true), m_abs(true), m_enginecheck(true), m_parking(true) {
-        timer = new QTimer(this);
-        timerIcons = new QTimer(this);
-        VehicleParser* carData = new VehicleParser("../../QtApp/replay/data/example_nurburgring_24h/data");
-        //qDebug() << "Working dir:" << QDir::currentPath();
-        carData->replayStart(); 
-        // Connect start button to replayStart method
-        // connect(this, &CircularBufferManagerWrapper::insertSignal, carData, &VehicleParser::replayStart);
-        connect(timer, &QTimer::timeout, this, &CircularBufferManagerWrapper::increment);
-        connect(timerIcons, &QTimer::timeout, this, &CircularBufferManagerWrapper::geartime);
-    }
+    explicit CircularBufferManagerWrapper(QObject *parent = nullptr) : QObject(parent), m_speed(160),
+    m_rpm(75), m_fuel(0), m_temp(0), m_coolanttemp(0), m_clock(0), m_enginetemp(0) {}
+    //CircularBufferManager<int> manager;
+    //std::vector<int> data = manager.consumeAll();
+    //std::vector<int> data = {speedVal};
 
-    double speed() const{
+    int speed() const{
         return m_speed;
     }
 
