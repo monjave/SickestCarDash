50e87fcbd02ad25c97bd28daed6ada0b179e7c4a
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-06-02 23:19:54 -0700

    correct ParserToBuffer integration test

--- QtApp/test/TestParserToBufferManager.cpp
+++ QtApp/test/TestParserToBufferManager.cpp
@@ -29,9 +29,24 @@ void TestParserToBufferManager::BuffersReceiveCorrectData(){
     QTest::qWait(40);
 
     CircularBufferManager* buffMan = parser.getBufferManager();
-    auto value = buffMan->peekBuffer(0);
-    QVERIFY(value.has_value());
-    QCOMPARE(value.value(), 0.15);
+    auto valSpeed = buffMan->peekBuffer(0); // speed
+    auto valThrottle = buffMan->peekBuffer(4); // throttle
+    auto valRPM = buffMan->peekBuffer(1); // rpms
+
+    QVERIFY(valSpeed.has_value());
+    QVERIFY(valThrottle.has_value());
+    QVERIFY(valRPM.has_value());
+
+    QCOMPARE(valSpeed.value(), 0.15);
+    QCOMPARE(valThrottle.value(), 50);
+    QCOMPARE(valRPM.value(), 15);
+
+    QTest::qWait(60);
+    auto valGear = buffMan->peekBuffer(6); // gear
+    QVERIFY(valGear.has_value());
+    QCOMPARE(valGear.value(), 1);
+
+    //QTest::qWait(20);
 }
 
 void TestParserToBufferManager::ReplayStopsWhenEmpty(){
@@ -53,3 +68,15 @@ void TestParserToBufferManager::createTestCSV(const std::string& path, const std
 
 QTEST_MAIN(TestParserToBufferManager)
 #include "TestParserToBufferManager.moc"
+
+/*
+      {"SPEED", {"010D", 0}},      // Buffer 0
+      {"RPM", {"010C", 1}},        // Buffer 1
+      {"FUEL", {"012F", 2}},       // Buffer 2
+      {"WATERTEMP", {"0105", 3}},  // Buffer 3
+      {"THROTTLE", {"0111", 4}},   // Buffer 4
+      {"OILTEMP", {"015C", 5}},    // Buffer 5
+      {"GEAR", {"01A4", 6}},       // Buffer 6
+      {"BATTVOLTS", {"0142", 7}},  // Buffer 7
+      {"STOREDDTC", {"03", 8}}     // Buffer 8
+*/
