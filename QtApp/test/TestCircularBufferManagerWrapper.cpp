40484b332cc8c002577211bfe7867b0b7d9bc490
Xavier <xaviersignup@gmail.com>
2025-06-09 11:14:44 -0700

    added some test code

--- QtApp/test/TestCircularBufferManagerWrapper.cpp
+++ QtApp/test/TestCircularBufferManagerWrapper.cpp
@@ -5,9 +5,20 @@ class CircularBufferManagerWrapperTest : public QObject {
     Q_OBJECT
 
 private slots:
-
-
+    void qPropertyTest();
 };
+
+void CircularBufferManagerWrapperTest::qPropertyTest() {
+    CircularBufferManagerWrapper* properties = new CircularBufferManagerWrapper();
+    properties->setSpeed(15.5);
+    QCOMPARE(properties->speed(), 15.5);
+
+    QSignalSpy spy(properties, &CircularBufferManagerWrapper::speedChanged);
+
+    properties->setSpeed(25.1);
+    properties->setSpeed(28.9);
+    QCOMPARE(spy.count(), 2);
+}
 
 QTEST_MAIN(CircularBufferManagerWrapperTest)
 #include "TestCircularBufferManagerWrapper.moc"
