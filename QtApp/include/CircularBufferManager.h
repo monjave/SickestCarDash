624e36a11f1dd468aee8306b5412b3cef840677a
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-06-14 11:34:27 -0700

    clean up  and move comments to header files

--- QtApp/include/CircularBufferManager.h
+++ QtApp/include/CircularBufferManager.h
@@ -8,40 +8,63 @@
 
 #ifndef CIRCULARBUFFERMANAGER_H
 #define CIRCULARBUFFERMANAGER_H
-
+#include <QObject>
 #include <vector>
 #include "CircularBuffer.h"
-#include <QObject>
 
 #define BUFFER_SIZE 1024
 #define BUFFERS_TO_INIT 9
 
 class CircularBufferManager : public QObject {
-    Q_OBJECT
+  Q_OBJECT
 
-private:
-    std::vector<CircularBuffer<double>> _buffers;
-    std::vector<double> _packet;
-    void writeToBuffer();
-    int _bufferByteSize;
-    int _bufferCount;
+ private:
+  std::vector<CircularBuffer<double>> _buffers;
+  std::vector<double> _packet;
+  void writeToBuffer();
+  int _bufferByteSize;
+  int _bufferCount;
 
-private slots:
+ private slots:
 
-signals:
-    void dataReady();
+ signals:
+  void dataReady();
 
-public:
-    CircularBufferManager(int numBuffers);
-    ~CircularBufferManager();
-    void publish(std::vector<double> values);
-    int8_t publish(double data, int buffIdx);
-    // std::vector<int> consumeAll();  // DEPRECATED PROBABLY
-    // void packetReady();  // Wrte this if we decide on event-driven
-    int getByteSize() const;
-    int getBufferCount() const;
-    std::optional<double> peekBuffer(int buffNum) const;
-    CircularBuffer<double>& accessBuffer(int buffNum);
+ public:
+  /// @brief Constructs a CircularBufferManager object to hold a number of
+  /// CircularBuffer objects.
+  /// @param numBuffers The number of CircularBuffer objects to initialize.
+  CircularBufferManager(int numBuffers);
+  ~CircularBufferManager();
+
+  /// @brief Push values to the CircularBuffer objects.
+  /// @param values A vector of values holding data that needs to be pushed to
+  /// the buffers.
+  void publish(std::vector<double> values);
+
+  /// @brief Push a single value to the CircularBuffer object.
+  /// @param data The data to be pushed to the CircularBuffer object.
+  /// @param buffIdx The index of the target buffer to be pushed to.
+  /// @return Returns a 0 if the publish was successful, otherwise return a 1;
+  int8_t publish(double data, int buffIdx);
+
+  /// @brief Get the combined byte size of all the buffers.
+  /// @return Returns the combined byte size of all the buffers.
+  int getByteSize() const;
+
+  /// @brief Get the number of buffers managed by the CircularBufferManager.
+  /// @return Returns the number of buffers.
+  int getBufferCount() const;
+
+  /// @brief Returns the most recent value for a given buffer.
+  /// @param buffNum The idx of the buffer to be accessed.
+  /// @return Returns the most recent value for a given buffer.
+  std::optional<double> peekBuffer(int buffNum) const;
+
+  /// @brief Access a buffer in the BufferManager.
+  /// @param buffNum The idx of the buffer to be accessed.
+  /// @return Returns the buffer stored at the buffNum idx in BufferManager.
+  CircularBuffer<double>& accessBuffer(int buffNum);
 };
 
-#endif //CIRCULARBUFFERMANAGER_H
+#endif  // CIRCULARBUFFERMANAGER_H
--- QtApp/src/CircularBufferManager.cc
+++ QtApp/src/CircularBufferManager.cc
@@ -1,7 +1,5 @@
 #include "CircularBufferManager.h"
 
-/// @brief Constructs a CircularBufferManager object to hold a number of CircularBuffer objects.
-/// @param numBuffers The number of CircularBuffer objects to initialize.
 CircularBufferManager::CircularBufferManager(int numBuffers) {
     _bufferCount = numBuffers;
     _bufferByteSize = 0;
@@ -11,28 +9,17 @@ CircularBufferManager::CircularBufferManager(int numBuffers) {
         _buffers.push_back(newBuffer);
         _bufferByteSize += sizeof(double) * BUFFER_SIZE;
     }
-
-    // TODO CONNECT THIS SHIT TO THE FRONT END :)
-    // connect(this, &CircularBufferManager::dataReady, &CircularBu)
 }
 
-/// @brief Get the combined byte size of all the buffers. 
-/// @return Returns the combined byte size of all the buffers.
 int CircularBufferManager::getByteSize() const {
 
     return _bufferByteSize;
 }
 
-/// @brief Get the number of buffers managed by the CircularBufferManager.
-/// @return Returns the number of buffers.
 int CircularBufferManager::getBufferCount() const {
     return _bufferCount;
 }
 
-/// @brief Push values to the CircularBuffer objects.
-/// @param values A vector of values holding data that needs to be pushed to the buffers.
-// TODO
-// If there is no value to update, then don't push anything to the idx'th buffer
 void CircularBufferManager::publish(std::vector<double> values) {
 
     if (_bufferCount != values.size()) {
@@ -43,10 +30,6 @@ void CircularBufferManager::publish(std::vector<double> values) {
     }
 }
 
-/// @brief Push a single value to the CircularBuffer object.
-/// @param data The data to be pushed to the CircularBuffer object.
-/// @param buffIdx The index of the target buffer to be pushed to.
-/// @return Returns a 0 if the publish was successful, otherwise return a 1;
 int8_t CircularBufferManager::publish(double data, int buffIdx) {
 
     if (buffIdx >= _bufferCount || buffIdx < 0) {
@@ -57,9 +40,6 @@ int8_t CircularBufferManager::publish(double data, int buffIdx) {
     return _buffers[buffIdx].push(data);
 }
 
-/// @brief Returns the most recent value for a given buffer.
-/// @param buffNum The idx of the buffer to be accessed.
-/// @return Returns the most recent value for a given buffer.
 std::optional<double> CircularBufferManager::peekBuffer(int buffNum) const {
 
     if (buffNum < 0 || buffNum >= _bufferCount) {
@@ -68,9 +48,6 @@ std::optional<double> CircularBufferManager::peekBuffer(int buffNum) const {
     return _buffers[buffNum].peek();
 }
 
-/// @brief Access a buffer in the BufferManager.
-/// @param buffNum The idx of the buffer to be accessed. 
-/// @return Returns the buffer stored at the buffNum idx in BufferManager.
 CircularBuffer<double>& CircularBufferManager::accessBuffer(int buffNum) {
 
     if (buffNum < 0 || buffNum >= _bufferCount) {
@@ -78,22 +55,5 @@ CircularBuffer<double>& CircularBufferManager::accessBuffer(int buffNum) {
     }
     return _buffers[buffNum];
 }
-
-/// @brief Get the most recent data from all buffers. 
-/// @return A vector of ints holding the most recent data from all buffers. Index 0 holds buffer 0's data, Index 1 holds buffer 1's data, ... , Index n holds buffer n's data.
-// std::vector<int> CircularBufferManager::consumeAll() {
-//     std::vector<int> packet;
-
-//     for (auto& buffer : _buffers) {
-//         std::optional<T> val = buffer.pop();
-//         if (val.has_value()) {
-//             packet.push_back(*val);
-//         } else {
-//             packet.push_back(-1);
-//         }
-//     }
-
-//     return packet;
-// }
 
 CircularBufferManager::~CircularBufferManager() = default;
