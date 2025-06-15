91cda56c34d68d7baab98c288c7c3d179ec65baa
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-06-14 17:54:35 -0700

    increase the polling sleep rate
    Co-authored-by: Xavier G <Nebulaics@users.noreply.github.com>
    Co-authored-by: Mo van Eeden <monjave@users.noreply.github.com>

--- QtApp/src/PIDPoller.cpp
+++ QtApp/src/PIDPoller.cpp
@@ -16,26 +16,26 @@ void PIDPoller::startPolling() {
             qDebug() << "PIDPoller calling run";
             emit requestPid("010D");  // Request Speed
             qDebug() << "Speed requested";
-            QThread::msleep(100);  // Small delay to avoid flooding the bus
+            QThread::msleep(200);  // Small delay to avoid flooding the bus
             emit requestPid("010C");  // Request RPM
             qDebug() << "RPM requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             emit requestPid("012F");  // Request Fuel Level
             qDebug() << "Fuel Level requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             emit requestPid("0105");  // Request Coolant Temp
             qDebug() << "Coolant Temp requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             //emit requestPid("0111"); Request Throttle Position, only needed for racing telemetry
             emit requestPid("015C");  // Request Oil Temp
             qDebug() << "Oil Temp requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             emit requestPid("01A4");  // Request Gear
             qDebug() << "Gear requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             emit requestPid("0142");  // Request Battery Voltage
             qDebug() << "Battery Voltage requested";
-            QThread::msleep(100);
+            QThread::msleep(200);
             emit requestPid("03");  // Request Stored DTCs
             qDebug() << "DTC requested";
 
