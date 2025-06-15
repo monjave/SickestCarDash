45a92ba6ab8095dd01d345bbe6c3bf438cd1fc6d
Domingo M <102837974+S-in-PNW@users.noreply.github.com>
2025-06-14 17:06:46 -0700

    Fix while loop
    Co-authored-by: Xavier G <Nebulaics@users.noreply.github.com>
    Co-authored-by: Mo van Eeden <monjave@users.noreply.github.com>

--- QtApp/src/VehicleConnection.cpp
+++ QtApp/src/VehicleConnection.cpp
@@ -103,7 +103,7 @@ void VehicleConnection::handleReadyRead() {
 
     int endIndex = buffer.indexOf('\r');
     qDebug() << "endIndex Value: " << endIndex;
-    while (endIndex != -1) {
+    while (true) {
         QByteArray responseBytes = buffer.left(endIndex);
         buffer.remove(0, endIndex + 1);  // Remove the processed response
         QString hexString = responseBytes.toHex(' ').toUpper();
